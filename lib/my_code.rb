ee5e9fbe11da3571539f53f70d9c2fa2b57a72d1
cesar mota <cesarmota@ymail.com>
2019-12-26 20:57:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,13 @@
 def map(array)
 new=[]
 counter=0 
-while counter<array.length do
+while counter<array.length 
   new.push(yield array[counter])
   counter += 1
 end
 new
+end
+
+
+def reduce()
 end
